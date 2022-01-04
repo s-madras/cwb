384090a3e2d95120cbc5ba869ea3635b441b17e5
SaminaAfrinDhola <faridpurweb@gmail.com>
2022-01-04 21:12:33 +0600

    this is theme

---
+++ header.php
@@ -0,0 +1,21 @@
+
+<?php
+   get_header();
+?>
+
+
+
+<nav>
+	<input type="checkbox" id="check" >
+		<label for="check" class="checkbtn">
+			<i class="fas fa-bars"></i>
+		</label>
+	<label for="contact" class="counter">
+	</label>
+	<label class="logo"><u>Child</u><span>WS</span></label>
+	<ul>
+		<li><a href="home.html">HOME</a></li>
+		<li><a href="flower.html">ITEMS</a></li>
+		<li><a href="about.html">ABOUT</a></li>
+	</ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Childhood website.</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?> https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri();?>/style.css">
+</head>
+<?php
+   get_header();
+?>
+<body>
+
+
+<i class="fas fa-cloud" style="font-size:60px;color:lightblue;"></i>
+
+</body>
+</html>
